2ae56f89cf40ec2707177734dcde5a72829bf907
jonas <jonas.alfredsson@protonmail.com>
2022-03-04 18:25:38 +0100

    Add GitHub Actions

---
+++ .github/version_extractor.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eo pipefail
+
+################################################################################
+#
+# This script will try to extract the version of the package of interest from
+# the file targeted.
+#
+# $1: The file to scan
+# $2: String defining if it is the server or client we are looking at
+#
+################################################################################
+
+
+version=$(sed -n -r -e 's/\s*devpi-'"${2}"'==([1-9]+\.[0-9]+\.[0-9]+).*$/\1/p' "${1}")
+
+if [ -z "${version}" ]; then
+    echo "Could not extract '${2}' version from '${1}'"
+    exit 1
+fi
+
+echo "::set-output name=APP_MAJOR::$(echo ${version} | cut -d. -f 1)"
+echo "::set-output name=APP_MINOR::$(echo ${version} | cut -d. -f 1-2)"
+echo "::set-output name=APP_PATCH::$(echo ${version} | cut -d. -f 1-3)"
