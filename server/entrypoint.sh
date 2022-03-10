cea5594df2bdd53f33986968dbca7c9f6e636f1b
jonas <jonas.alfredsson@protonmail.com>
2022-03-10 13:52:41 +0100

    Simplify log printing in entrypoint

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,28 +1,33 @@
 #!/bin/sh
 set -e
 
+log() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S,000') $1 [entrypoint] $2"
+}
+
+if [ -z "${DEVPI_PASSWORD}" ]; then
+    log "ERROR" "Root password cannot be empty"
+    exit 1
+fi
+
 # Execute any potential shell scripts in the entrypoint.d/ folder.
 find "/entrypoint.d/" -follow -type f -print | sort -V | while read -r f; do
     case "${f}" in
         *.sh)
             if [ -x "${f}" ]; then
-                echo "$(date '+%Y-%m-%d %H:%M:%S,000') INFO [entrypoint] Launching ${f}";
+                log "INFO" "Launching ${f}";
                 "${f}"
             else
-                echo "$(date '+%Y-%m-%d %H:%M:%S,000') INFO [entrypoint] Ignoring ${f}, not executable";
+                log "INFO" "Ignoring ${f}, not executable";
             fi
             ;;
         *)
-            echo "$(date '+%Y-%m-%d %H:%M:%S,000') INFO [entrypoint] Ignoring ${f}";;
+            log "INFO" "Ignoring ${f}";;
     esac
 done
 
 # Initialize devpi if there is no indication of it being run before.
 if [ ! -f "${DEVPISERVER_SERVERDIR}/.serverversion" ]; then
-    if [ -z "${DEVPI_PASSWORD}" ]; then
-        echo "$(date '+%Y-%m-%d %H:%M:%S,000') ERROR [entrypoint] Root password cannot be empty"
-        exit 1
-    fi
     devpi-init --root-passwd "${DEVPI_PASSWORD}"
 fi
 
